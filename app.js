beb3aeed05b3ff4a09beb390fe5c395a5cbdd1b3
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-08-13 15:00:28 +0200

    handling POST requests

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json()); // this is a middleware
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(404)
@@ -17,14 +19,43 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// GET
+
+// always specify the version of the API
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
+    results: tours.length, // only use whenever you are sending an array of data
     data: {
       tours,
     },
   });
 });
+
+// POST
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      }); // 201 stands for created
+    }
+  );
+});
+
+// PORT
 
 const port = 3001;
 app.listen(port, () => {
