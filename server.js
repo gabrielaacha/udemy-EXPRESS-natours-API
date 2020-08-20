2111dc0e01abceae960874fe01f90b94f18699f9
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-08-20 14:27:51 +0200

    mongoose forst steps

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-// console.log(process.env);
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
